d50c6a73453a0026071f5d304bd1ab2500346c66
dev0ps221 <seybaxscorpion@gmail.com>
2022-12-31 01:35:10 +0000

    entitor:building modules logic

--- php/core/mods/entites.php
+++ php/core/mods/entites.php
@@ -4,6 +4,28 @@
         function createNew($titre){
             return $this->db->request('insert_into_entites',['titre'=>$titre]);
         }
+        function delete($id){
+            return $this->db->request('delete_entites_entry',$id);
+        }
+        function deleteAll(){
+            return $this->db->request('delete_entites_entries',$id);
+        }
+        function deletebytitre($titre){
+            return $this->db->request('delete_entites_entry_by_titre',"'$titre'");
+        }
+        function select($id){
+            return $this->db->request('select_entites_entry',$id);
+        }
+        function selectAll($id){
+            return $this->db->request('select_entites_entries',$id);
+        }
+        function selectbytitre($titre){
+            return $this->db->request('select_entites_entry_by_titre',"'$titre'");
+        }
+        function updatefield($id,$field,$value){
+            $action = "update_entites_$field";
+            return $this->db->request($action,"'$value'");    
+        }
         function __construct($name,$conn){
             parent::__construct($name,$conn);
         }
--- php/index.php
+++ php/index.php
@@ -2,7 +2,5 @@
     include('core/entitor.php');
     $entitor        = new Entitor([]);
     $entites        = $entitor->getmod('entites');
-    $champs_entites = $entitor->getmod('champs_entites');
-    $entrees_champs = $entitor->getmod('entrees_champs');
-    $entites->createNew('tableau un');
+    
 ?>
