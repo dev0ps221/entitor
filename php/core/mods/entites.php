ca1d3db04ce43004e0e5e5c849d7b630948e6fae
dev0ps221 <seybaxscorpion@gmail.com>
2022-12-31 01:37:08 +0000

    entitor:building objects logic

--- php/core/mods/entites.php
+++ php/core/mods/entites.php
@@ -1,5 +1,6 @@
 <?php
     include_once(dirname(__FILE__)."/modskel.php");
+     
     class EntitorEntites extends EntitorModule{
         function createNew($titre){
             return $this->db->request('insert_into_entites',['titre'=>$titre]);
---
+++ php/core/objects/entite.php
@@ -0,0 +1,7 @@
+<?php
+    class EntitorEntite{
+        function __construct(){
+            
+        }
+    }
+?>
