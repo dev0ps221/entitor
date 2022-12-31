79a07c425b67a77cff47b864dafd95477248c3ac
dev0ps221 <seybaxscorpion@gmail.com>
2022-12-31 13:00:56 +0000

    lignes

--- php/core/objects/entite.php
+++ php/core/objects/entite.php
@@ -2,15 +2,15 @@
     include_once(dirname(__FILE__)."/objskel.php");
     class EntitorEntite extends EntitorObject{
         private $champs = null;
-        function addligne(){
+        function addligne($data){
             return $this->lignes->createNew($this->get('id'));
         }
         function addchamps($titre,$type){
-            return $this->lignes->createNew($titre,$this->get('id'),$type);
+            return $this->champs->createNew($titre,$this->get('id'),$type);
         }
         function __construct($manager,$data){
             parent::__construct($manager,$data);
-            $this->lignes = $manager->entitor->getmod('lignes');
+            $this->lignes = $manager->entitor->getmod('entrees_lignes');
             $this->champs = $manager->entitor->getmod('champs_entites');
         }
     }
