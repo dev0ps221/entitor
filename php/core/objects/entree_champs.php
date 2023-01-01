2db9b743dcb04e8fe6ba338dd3d689043ae4c9d6
dev0ps221 <seybaxscorpion@gmail.com>
2023-01-01 18:51:35 +0000

    lignes:entrees

--- php/core/objects/entree_champs.php
+++ php/core/objects/entree_champs.php
@@ -1,9 +1,15 @@
 <?php
     include_once(dirname(__FILE__)."/objskel.php");
     class EntitorEntreeChamps extends EntitorObject{
+        function update($fields=[]){
+            foreach ($fields as $key => $field) {
+                $value = $fields[$field];
+                $udpdatefunc = "update_entrees_champs_entites_$field";
+                echo $udpdatefunc; 
+            }
+        }
         function __construct($manager,$data){
             parent::__construct($manager,$data);
-            print_r($data);
         }
     }
 ?>
