011302a813cdf7fece156460687f293a57c011db
dev0ps221 <seybaxscorpion@gmail.com>
2023-01-04 02:19:13 +0000

    entitorbuilder

--- php/builder.js
+++ php/builder.js
@@ -13,9 +13,25 @@ class EntitorBuilder{
     ajouter_champs(event){
 
     }
-    addchampsrender(){
-        const ajouter_champs = this.ajouter_champs
-        const render = `<div id='ajouter_champs'>
+    ajouter_champs_elem(){
+        const ajouter_champs = document.createElement('div')
+        ajouter_champs.id = 'ajouter_champs'
+        const label_ajouter_champs = document.createElement('h3')
+        label_ajouter_champs.innerHTML = 'créer un champs' 
+        label_ajouter_champs.classList.
+        const optionbox = document.createElement('div')
+        optionbox.id = 
+        optionbox.classList.
+        const label_titre = document.createElement('label')
+        label_titre.id = 
+        label_titre.classList.
+        const titre = document.createElement('input')
+        titre.id = 
+        titre.classList.
+        const action = document.createElement('button')
+        action.id = 
+        action.classList.
+        const addchamps : `<div id='ajouter_champs'>
                 <h3>
                     creer un champs
                 </h3>
