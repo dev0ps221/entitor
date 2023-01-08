469c792d4ea5ac54274bd7fb15c2d399b7a97e46
dev0ps221 <seybaxscorpion@gmail.com>
2023-01-08 15:29:59 +0000

    sheets

--- php/core/objects/sheet.php
+++ php/core/objects/sheet.php
@@ -7,9 +7,7 @@
         function setup(){
             $this->learnDimensions();
             $this->setMap();
-            echo "<pre>";
-                print_r($this->map);
-            echo "<pre>";
+            $this->fillMap($this->entite);
         }
         function setMap(){
             $x = 0;
@@ -22,6 +20,30 @@
                }
                $x = 0;
                $y++; 
+            }
+        }
+        function fillMap($entite){
+            $x = 0;
+            $y = 0;
+            $lignes = $entite->getlignes();
+            while($y < $this->height){
+                $entrees = $lignes[$y]->getentree();
+                foreach($entrees as $entree){
+                    if($entree->get('type') == 'entree'){
+                        $entreesentree = $this->manager->entitor->getmod('lignes')->select($entree->get('valeur'))->getentree();
+                        foreach($entreesentree as $entree){
+                            $this->map[$y][$x]['coords'] = chr(ord('a')+($x)).":".($y+1);    
+                            $this->map[$y][$x]['valeur'] = $entree->get('valeur');    
+                            $x++;
+                        } 
+                    }else{
+                        $this->map[$y][$x]['coords'] = chr(ord('a')+($x)).":".($y+1);
+                        $this->map[$y][$x]['valeur'] = $entree->get('valeur');
+                        $x++;
+                    }
+                }
+                $x = 0;
+                $y++;
             }
         }
         function countlignes(){
